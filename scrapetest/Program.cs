8ef7b72a1915fd55a159d2337c79c240f0fe3cb1
Máté Fejes <88377040+fema3832@users.noreply.github.com>
2022-09-03 22:35:39 +0200

    main

---
+++ scrapetest/Program.cs
@@ -0,0 +1,62 @@
+﻿using HtmlAgilityPack;
+using System;
+
+namespace ScrapeTest
+{
+    static class Program
+    {
+        static void Main()
+        {
+            int hits = 0;
+            List<string> notAvaliable = new();
+            var currentDirectory = Environment.CurrentDirectory;
+
+            Thread thread1 = new Thread(() => checkID());
+            Thread thread2 = new Thread(() => checkID());
+            Thread thread3 = new Thread(() => checkID());
+            Thread thread4 = new Thread(() => checkID());
+            Thread thread5 = new Thread(() => checkID());
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
+            thread4.Start();
+            thread5.Start();
+
+            Console.Title = "Steam lowid checker | by fema3832 | Current hits: 0";
+
+            void checkID() {
+                while (true) {
+                    string randomID = scrapetest.createRandom.random_string(3);
+                    if (!notAvaliable.Contains(randomID))
+                    {
+                        Thread.Sleep(100);
+                        HtmlWeb web = new HtmlWeb();
+                        HtmlDocument doc = web.Load($"https://steamcommunity.com/id/{randomID}");
+
+                        try
+                        {
+                            if (doc.DocumentNode.SelectSingleNode("//*[@id=\"responsive_page_template_content\"]/div").GetAttributeValue("class", "default") == "error_ctn")
+                            {
+                                using (StreamWriter writer = new StreamWriter($"{currentDirectory}/avaiable.txt", true)){ writer.WriteLine($"{randomID} is available!"); }
+                                Console.WriteLine($"{randomID} is available!");
+                                hits++;
+
+                                Console.Title = $"Steam lowid checker | by fema3832 | Current hits: {hits.ToString()}\n";
+                            }
+                            else
+                            {
+                                Console.WriteLine($"{randomID} is not available!");
+                                notAvaliable.Add(randomID);
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex);
+                            checkID();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ scrapetest/createRandom.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace scrapetest
+{
+    public static class createRandom
+    {
+        public static string random_string(int length)
+        {
+            Random ran = new Random();
+
+            const string b = "abcdefghijklmnopqrstuvwxyz0123456789";
+            string random = "";
+
+            for (int i = 0; i < length; i++)
+            {
+                int a = ran.Next(b.Length); //string.Lenght gets the size of string
+                random += b[a];
+            }
+
+            return random;
+        }
+    }
+}
